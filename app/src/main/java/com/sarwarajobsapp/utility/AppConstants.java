6e03647059ed56ec38ab1823f69bf650f0069778
rahul <malhotrarahul971@gmail.com>
2024-10-21 19:25:19 +0530

    sarawarajobs-14+url

--- app/src/main/java/com/sarwarajobsapp/utility/AppConstants.java
+++ app/src/main/java/com/sarwarajobsapp/utility/AppConstants.java
@@ -4,7 +4,9 @@ public class AppConstants {
 
     //Final base url
 
-    public static String apiUlr="https://sarwarajobs.com/api/v1/app/";
+    //public static String apiUlr="https://sarwarajobs.com/api/v1/app/";
+
+    public static String apiUlr="https://career-finder.co.in/";
 
     public static  String logindata="logindata";
 
