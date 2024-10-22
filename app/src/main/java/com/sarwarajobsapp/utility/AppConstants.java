609474db8a906fd64fedd2984790843b382d40f1
User <tstrkumar@gmail.com>
2024-10-22 15:08:11 +0530

    url+applogo+splash

--- app/src/main/java/com/sarwarajobsapp/utility/AppConstants.java
+++ app/src/main/java/com/sarwarajobsapp/utility/AppConstants.java
@@ -4,8 +4,10 @@ public class AppConstants {
 
     //Final base url
 
-    public static String apiUlr="https://sarwarajobs.com/api/v1/app/";
+   // public static String apiUlr="https://sarwarajobs.com/api/v1/app/";
 
+
+    public static String apiUlr=" https://career-finder.co.in/";
     public static  String logindata="logindata";
 
 
